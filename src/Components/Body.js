0764413ecfaf6148a2d8b76d3f222d47f92078bd
sh4f3e <shafeess46@gmail.com>
2024-05-28 10:51:19 +0530

    fix

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -16,10 +16,11 @@ const Body = () => {
   useEffect(() => {
     fetchData();
   }, []);
+  //`http://localhost:3000/api/restaurants?lat=11.9415915&lng=79.8083133&page_type=DESKTOP_WEB_LISTING`
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.9415915&lng=79.8083133&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      `http://localhost:3000/api/restaurants?lat=11.9415915&lng=79.8083133&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
     );
 
     const json = await data.json();
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,7 +1,10 @@
-export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4YT9VAq6nNt8nUf8yXLSCngNaskO1s2_D9MAwvBjOQw&s";
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4YT9VAq6nNt8nUf8yXLSCngNaskO1s2_D9MAwvBjOQw&s";
 
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.9415915&lng=79.8083133&restaurantId=";
+export const MENU_URL = `http://localhost:3000/api/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.9415915&lng=79.8083133&restaurantId=`;
 
-export const SEARCH_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png"
+export const SEARCH_URL =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png";
